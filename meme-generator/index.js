17f46a2efc27bc836d7ac8d62e0e2c94c2f66afa
Trcksyy69 <quingames8@gmail.com>
2020-10-26 12:49:27 +0100

    added generate button

--- meme-generator/index.js
+++ meme-generator/index.js
@@ -24,6 +24,20 @@ function renderText (id) {
         document.querySelector(id).innerHTML = text;
     }
 }
+const btn= document.querySelector('button')
+btn.addEventListener("click", generateMeme)
+function generateMeme (){
+    html2canvas(document.querySelector(".meme-container"), {
+        useCORS: true, //By passing this option in function Cross origin images will be rendered properly in the downloaded version of the PDF
+        allowTaint: true
+    })
+    .then(canvas => {
+        // var img = canvas.toDataURL()
+        //    window.open(img);
+        document.body.appendChild(canvas)
+    });
+}
+
 
 // const lijst = [1,2,3,4,5,6,7]
 // const lijstPlus5 = lijst.map(plus5)
